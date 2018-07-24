a7e39e6121c02316a5a934105a01c7170e3676d0
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-07-24 00:55:50 -0700

    removed  unneeded files

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const fs = require('fs')
 const keys = require("./keys.js");
 const request = require('request');
 const Spotify = require('node-spotify-api');
@@ -11,25 +12,25 @@ function searchOMDB() {
   if (input == '') {
     input = "Mr. Nobody";
   }
-  
+
   let queryUrl = 'http://www.omdbapi.com/?t=' + input + '&y=&plot=short&apikey=trilogy';
 
   request(queryUrl, function (error, response, body) {
     if (!error) {
 
-        let title = `\nTitle: ${JSON.parse(body).Title}`;
-        let year = `\nYear Released: ${JSON.parse(body).Year}`;
-        let imdb = `\nIMDB Rating: ${JSON.parse(body).imdbRating}`;
-        let rotten = `\nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`;
-        let country = `\nCountry produced: ${JSON.parse(body).Country}`;
-        let lang = `\nLanguage: ${JSON.parse(body).Language}`;
-        let plot = `\nPlot: ${JSON.parse(body).Plot}`;
-        let actors = `\nActors: ${JSON.parse(body).Actors}`;
+      let title = `\nTitle: ${JSON.parse(body).Title}`;
+      let year = `\nYear Released: ${JSON.parse(body).Year}`;
+      let imdb = `\nIMDB Rating: ${JSON.parse(body).imdbRating}`;
+      let rotten = `\nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`;
+      let country = `\nCountry produced: ${JSON.parse(body).Country}`;
+      let lang = `\nLanguage: ${JSON.parse(body).Language}`;
+      let plot = `\nPlot: ${JSON.parse(body).Plot}`;
+      let actors = `\nActors: ${JSON.parse(body).Actors}`;
 
-      console.log(title, year, imdb, rotten, country, lang, plot, actors) 
+      console.log(title, year, imdb, rotten, country, lang, plot, actors)
     } else {
-    console.log('error: ', error); // Print the error if one occurred
-  }
+      console.log('error: ', error); // Print the error if one occurred
+    }
   });
 }
 
@@ -38,43 +39,49 @@ function searchSpotify(input) {
   console.log(input)
   console.log('searching spotify only')
   var spotify = new Spotify(keys.spotify);
-  
+
   if (input == '') {
     spotify
-    .search({type: 'track', query: 'the sign'})
-     
-      .then(function (data) {          
-        
-       // console.log(JSON.stringify(data.tracks.items[0].album.name));
-  
+      .search({
+        type: 'track',
+        query: 'the sign'
+      })
+
+      .then(function (data) {
+
+        // console.log(JSON.stringify(data.tracks.items[0].album.name));
+
         let artist = `\nArtist: ${JSON.stringify(data.tracks.items[5].artists[0].name)}`;
         let songName = `\nSong Name: ${JSON.stringify(data.tracks.items[5].name)}`;
         let preview = `\nSong Preview: ${JSON.stringify(data.tracks.items[5].album.artists[0].external_urls.spotify)}`;
         let album = `\nAlbum: ${JSON.stringify(data.tracks.items[5].album.name)}`
-        
+
         console.log(artist, songName, preview, album);
       })
       .catch(function (err) {
         console.error('Spotify error occurred: ' + err);
       });
   } else {
-  spotify
-  .search({type: 'track', query: input})
-   
-    .then(function (data) {          
-      
-      //console.log(JSON.stringify(data.tracks.items[0]));
+    spotify
+      .search({
+        type: 'track',
+        query: input
+      })
 
-      let artist = `\nArtist: ${JSON.stringify(data.tracks.items[0].artists[0].name)}`;
-      let songName = `\nSong Name: ${JSON.stringify(data.tracks.items[0].name)}`;
-      let preview = `\nSong Preview: ${JSON.stringify(data.tracks.items[0].album.artists[0].external_urls.spotify)}`;
-      let album = `\nAlbum: ${JSON.stringify(data.tracks.items[0].album.name)}`
-      
-      console.log(artist, songName, preview, album);
-    })
-    .catch(function (err) {
-      console.error('Spotify error occurred: ' + err);
-    });
+      .then(function (data) {
+
+        //console.log(JSON.stringify(data.tracks.items[0]));
+
+        let artist = `\nArtist: ${JSON.stringify(data.tracks.items[0].artists[0].name)}`;
+        let songName = `\nSong Name: ${JSON.stringify(data.tracks.items[0].name)}`;
+        let preview = `\nSong Preview: ${JSON.stringify(data.tracks.items[0].album.artists[0].external_urls.spotify)}`;
+        let album = `\nAlbum: ${JSON.stringify(data.tracks.items[0].album.name)}`
+
+        console.log(artist, songName, preview, album);
+      })
+      .catch(function (err) {
+        console.error('Spotify error occurred: ' + err);
+      });
   }
 }
 
@@ -82,17 +89,20 @@ function searchTwitter() {
 
   var client = new Twitter(keys.twitter);
 
-  client.get('search/tweets', {q: 'kthoma1984' , count: 21}, function (error, tweets, response) {
+  client.get('search/tweets', {
+    q: 'kthoma1984',
+    count: 21
+  }, function (error, tweets, response) {
     console.log("searching tweets only")
     if (!error) {
       //console.log(tweets);
       //console.log(response);
 
-      for (let i=0; i<21; i++) {
-      let tweetCreated = `\nCreated: ${tweets.statuses[i].created_at}`;
-      let myTweet = `\nTweet: ${tweets.statuses[i].text}`;
+      for (let i = 0; i < 21; i++) {
+        let tweetCreated = `\nCreated: ${tweets.statuses[i].created_at}`;
+        let myTweet = `\nTweet: ${tweets.statuses[i].text}`;
 
-      console.log(tweetCreated, myTweet);
+        console.log(tweetCreated, myTweet);
       }
     } else {
       console.log(JSON.stringify(error));
@@ -112,6 +122,9 @@ function searchCat(search, input) {
       searchSpotify(input);
       break;
     case 'movie-this':
+      searchOMDB(input);
+      break;
+    case 'do-what-it-says':
       searchOMDB(input);
       break;
     default:
